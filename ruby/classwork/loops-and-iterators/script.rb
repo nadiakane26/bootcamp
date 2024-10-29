f38dab6170bb67703b42287ad1730697a09eac21
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-10-29 13:14:06 -0500

    Added while loop

--- ruby/classwork/loops-and-iterators/script.rb
+++ ruby/classwork/loops-and-iterators/script.rb
@@ -1,11 +1,25 @@
-=begin Write a program that prints the numbers from 1 to 20. 
+=begin 
+Write a program that prints the numbers from 1 to 20. 
 For each number, if it is even, print "Even," and if it is odd, print "Odd."
 =end
 
+
+# For Loop
 for num in 1..20
     if num % 2 == 0
         puts "#{num} : Even"
     else
         puts "#{num} : Odd"
     end
+end
+
+# While Loop
+num = 1
+while num <= 20
+    if num % 2 == 0
+        puts "#{num} : Even"
+    else
+        puts "#{num} : Odd"
+    end
+  num += 1
 end
