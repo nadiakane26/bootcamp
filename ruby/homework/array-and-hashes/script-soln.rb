c6cc40e65bc1c4e15f53dcae4d4e49bfa84afad5
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-10-31 10:30:03 -0500

    Array and Hashes in Ruby Solutions

---
+++ ruby/homework/array-and-hashes/script-soln.rb
@@ -0,0 +1,91 @@
+##########################
+# Task 1: Arrays
+##########################
+# Create an array 
+fruit = [ "apple", "banana", "cherry", "date", "fig", "grape"]
+
+# Print the entire array.
+puts "Fruits: #{fruit}"
+
+# Print the first and last elements of the array.
+puts "First element: #{fruit.first}, Last element: #{fruit.last}"
+
+# Add "kiwi" to the end of the array.
+fruit << "kiwi"
+
+# Remove "cherry" from the array.
+fruit.delete("cherry")
+
+# Print the modified array.
+puts "Modified Fruits: #{fruit}"
+
+# Check if "apple" is in the array and print the result.
+puts "Is 'apple in the array? #{fruit.include?("apple")? "Yes" : "No"}"
+
+# Calculate and print the total number of fruits in the fruits array.
+puts "Total number of fruits: #{fruit.length}" 
+
+#### Stretch Goal ####
+
+# Ask the user to input a fruit
+print "Fruit please: "
+fruit_input = gets.chomp.downcase
+
+# Check if that fruit is in the array. Print the result.
+fruit.include?(fruit_input) ? (puts fruit_input) : (puts "#{fruit_input} is not in the array")
+
+# Sort the fruits array in alphabetical order and print the sorted list.
+puts "Sorted fruit: #{fruit.sort}"
+
+##########################
+# Task 2: Hashes
+##########################
+student = { 
+    "name" => "John Smith",
+    "age" => 25,
+    "major" => "Computer Science"
+}
+
+# Print the entire hash.
+puts "Student Info: #{student}"
+
+# Print only the value associated with the "name" key.
+puts "Student Name: #{student["name"]}"
+
+# Add a new key-value pair to the hash: "gpa" => 3.7.
+student["gpa"] = 3.7
+
+# Update the "age" value to 26.
+student["age"] = 26
+
+# Print the modified hash.
+puts "Modified Student Info: #{student}"
+
+# Check if the hash contains a key "gender" and print the result.
+puts "Does the hash contain a 'gender' key: #{student.key?("gender")? "Yes" : "No"}"
+
+#### Stretch Goal ####
+
+# A new array that contains multiple student hashes
+students = [
+  {
+    "name" => "John Smith",
+    "age" => 25,
+    "major" => "Computer Science",
+    "gpa" => 3.7
+  },
+  {
+    "name" => "Vernor Palfrie",
+    "age" => 22,
+    "major" => "Mathematics",
+    "gpa" => 3.9
+  },
+  {
+    "name" => "Alfie Bilton;",
+    "age" => 23,
+    "major" => "Biology",
+    "gpa" => 3.4
+  }
+]
+# Print the student name and major
+students.each { |student| puts "#{student["name"]}: #{student["major"]}" }
