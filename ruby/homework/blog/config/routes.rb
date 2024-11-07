c47968f0ffa4b92cdf934bcb5f9c252272facb36
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-07 13:59:04 -0600

    Added Category MVC

--- ruby/homework/blog/config/routes.rb
+++ ruby/homework/blog/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "categories/index"
+  get "categories/show"
   root "articles#index"
+  resources :categories, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,7 +16,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :category do
+  end
   resources :articles do
     resources :comments
+  
   end
+
+  
 end
