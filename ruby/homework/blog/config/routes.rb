66ec3f7bf120ba8eca20a059143b64a8eef19482
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-07 15:00:20 -0600

    Added 10. Deleting Comments

--- ruby/homework/blog/config/routes.rb
+++ ruby/homework/blog/config/routes.rb
@@ -16,8 +16,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :category do
-  end
+  resources :category
+
+
   resources :articles do
     resources :comments
   
