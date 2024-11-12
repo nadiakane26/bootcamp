24c5f3d8d4cf12f72b5418f5b4ac4484e11876e9
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-11 18:47:23 -0600

    setting up devise

--- ruby/classwork/devisemailer/config/importmap.rb
+++ ruby/classwork/devisemailer/config/importmap.rb
@@ -1,7 +1,6 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
+# config/importmap.rb
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "controllers"
+pin "@rails/ujs", to: "rails-ujs.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
+pin "bootstrap", to: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
