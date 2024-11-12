5b9932be56c815dac33dd40b928dce09cdb53b09
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-12 01:32:01 -0600

    Version 8.0.0 Getting Started with Rails On My Own

--- ruby/classwork/blog/app/controllers/concerns/visible.rb
+++ ruby/classwork/blog/app/controllers/concerns/visible.rb
@@ -6,7 +6,7 @@ module Visible
     included do
       validates :status, inclusion: { in: VALID_STATUSES }
     end
-    # display a count of public articles or comments on our main page
+  
     class_methods do
       def public_count
         where(status: "public").count
--- ruby/classwork/blog/config/routes.rb
+++ ruby/classwork/blog/config/routes.rb
@@ -1,24 +1,7 @@
-# This block begins the definition of all your routes in the application. 
-# It’s a method call that tells Rails to set up routing rules.
 Rails.application.routes.draw do
   root "articles#index"
-  # A route that responds to HTTP GET requests made to the URL /articles
-  # The request should be handled by the index action of the ArticlesController
-  
-  # The GET /articles request (typically a request to show a list of articles) will be routed to the index action in the ArticlesController.
-  # Index Action: Gather the necessary data (like all articles in the database) and pass it to the view, which will then display it to the user.
-  
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
 
-  # replaced the two get routes
-  resources :articles
-
-  # This creates comments as a nested resource within articles. 
-  # hierarchical relationship that exists between articles and comments.
   resources :articles do
     resources :comments
-  end 
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
