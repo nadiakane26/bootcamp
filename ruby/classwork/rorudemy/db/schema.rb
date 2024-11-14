e279f6ba1ceb5f5392d04a85f62ac1166e704560
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-13 23:13:08 -0600

    scaffold crud articles

--- ruby/classwork/rorudemy/db/schema.rb
+++ ruby/classwork/rorudemy/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_14_042917) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_14_044641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
