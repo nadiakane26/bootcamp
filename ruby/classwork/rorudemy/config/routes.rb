226a0608ff744e5ecad1ddad749ccb64f13ee8b0
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-13 22:44:38 -0600

    updated navbar and user registration and edit

--- ruby/classwork/rorudemy/config/routes.rb
+++ ruby/classwork/rorudemy/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: "pages#index"
   # get "hello" to "pages/contact"
   get  "pages/contact"
--- ruby/classwork/rorudemy/db/schema.rb
+++ ruby/classwork/rorudemy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_14_025627) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_14_042917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,9 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_14_025627) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
