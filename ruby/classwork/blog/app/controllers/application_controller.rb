398355191d0abe45b4eb98fdce1a91a1c0178c07
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-20 09:23:08 -0600

    Welcome Back, current_user

--- ruby/classwork/blog/app/controllers/application_controller.rb
+++ ruby/classwork/blog/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Welcome back, #{current_user.first_name}!"
+    root_path # Redirect to the home page
+  end
   protected
 
   def configure_permitted_parameters
