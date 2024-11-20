89aa5bf83f8d5b09d65e3101eacd6b4e0ffdf9eb
Nadia Kane <nadiakane@Nadias-MacBook-Air.local>
2024-11-20 15:19:35 -0600

    Fixed Delete Author, Article, Category bug, No more turbo

--- ruby/classwork/blog/app/javascript/application.js
+++ ruby/classwork/blog/app/javascript/application.js
@@ -2,5 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+//= require jquery
+//= require bootstrap-sprockets
 
 import {loadStripe} from '@stripe/stripe-js';
--- ruby/classwork/blog/db/schema.rb
+++ ruby/classwork/blog/db/schema.rb
@@ -91,10 +91,10 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_20_050645) do
     t.string "stripe_customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "unconfirmed_email"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
